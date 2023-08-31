5566cd4797364f6a4e8a1f4711395e6172f5e3af
MiBeloz <miha_bmv_@mail.ru>
2023-09-01 01:32:37 +0600

    fix Data_DrySolid.hpp

--- src/Data_DrySolid.hpp
+++ src/Data_DrySolid.hpp
@@ -1,86 +1,17 @@
 #pragma once
 
-
-//class MyData {
-//public:
-//	enum class MyFormat {
-//		kText,
-//		kHTML,
-//		kJSON
-//	};
-//
-//	MyData(const std::string data) : m_data(data) {}
-//
-//	MyFormat SetNextFormat(MyFormat nextFormat) noexcept {
-//		mp_nextFormat = nextFormat;
-//		return mp_nextFormat;
-//	}
-//
-//	virtual std::string print(MyFormat format = MyFormat::kText) noexcept {
-//		MyData::print(mp_nextFormat);
-//		return {};
-//	}
-//
-//protected:
-//	std::string m_data;
-//
-//private:
-//	MyFormat mp_nextFormat = MyFormat::kText;
-//};
-//
-//class Text_String : public MyData {
-//public:
-//	std::string print(MyFormat format) override {
-//		if (m_format == MyFormat::kText) {
-//			return m_data;
-//		}
-//		MyData::SetNextFormat(MyFormat::kHTML);
-//		return MyData::print();
-//	}
-//
-//private:
-//	MyFormat m_format = MyFormat::kText;
-//};
-//
-//class HTML_String : public MyData {
-//public:
-//	std::string print(MyFormat format) override {
-//		if (m_format == MyFormat::kHTML) {
-//			return m_data;
-//		}
-//		MyData::SetNextFormat(MyFormat::kJSON);
-//		return MyData::print();
-//	}
-//
-//private:
-//	MyFormat m_format = MyFormat::kHTML;
-//};
-//
-//class JSON_String : public MyData {
-//public:
-//	std::string print(MyFormat format) override {
-//		if (m_format == MyFormat::kJSON) {
-//			return m_data;
-//		}
-//		MyData::SetNextFormat(MyFormat::kText);
-//		return MyData::print();
-//	}
-//
-//private:
-//	MyFormat m_format = MyFormat::kJSON;
-//};
-
-
-enum class MyFormat {
-	kText,
-	kHTML,
-	kJSON
-};
+#include <string>
 
 class MyData {
 public:
-	virtual std::string print(MyFormat format = MyFormat::kText) const noexcept {
-		return "Ошибка формата!";
+	enum class MyFormat {
+		kText,
+		kHTML,
+		kJSON
+	};
+
+	virtual std::string print() const noexcept {
+		return "Ошибка формата! Формат не задан.";
 	};
 
 	virtual ~MyData() = default;
@@ -89,52 +20,65 @@ protected:
 	std::string m_data;
 };
 
-//class DefaultHandler : public Handler {
-//public:
-//	bool print(int request) override {
-//		printf("%d is prime\n", request);
-//		return false;
-//	}
-//};
-
 class Text : public MyData {
 public:
-	explicit Text(const std::string& data) {
+	explicit Text(const std::string& data) : MyData() {
 		m_data = data;
 	}
 
-	std::string print(MyFormat format) const noexcept override {
-		if (format == MyFormat::kText)
+	MyData::MyFormat getFormat() {
+		return m_format;
+	}
+
+	std::string print() const noexcept override {
+		if (m_format == MyFormat::kText)
 			return m_data;
 		else
-			return MyData::print(format);
+			return MyData::print();
 	}
+
+private:
+	MyData::MyFormat m_format = MyData::MyFormat::kText;
 };
 
 class HTML : public MyData {
 public:
-	explicit HTML(const std::string& data) {
+	explicit HTML(const std::string& data) : MyData() {
 		m_data = data;
 	}
 
-	std::string print(MyFormat format) const noexcept override {
-		if (format == MyFormat::kHTML)
+	MyData::MyFormat getFormat() {
+		return m_format;
+	}
+
+	std::string print() const noexcept override {
+		if (m_format == MyFormat::kHTML)
 			return "<html>" + m_data + "<html/>";
 		else
-			return MyData::print(format);
+			return MyData::print();
 	}
+
+private:
+	MyData::MyFormat m_format = MyData::MyFormat::kHTML;
 };
 
 class JSON : public MyData {
 public:
-	explicit JSON(const std::string& data) {
+	explicit JSON(const std::string& data) : MyData() {
 		m_data = data;
 	}
 
-	std::string print(MyFormat format) const noexcept override {
-		if (format == MyFormat::kJSON)
-			return "{ \"data\": \"" + m_data + "\"}";
-		else
-			return MyData::print(format);
+	MyData::MyFormat getFormat() {
+		return m_format;
 	}
+
+	std::string print() const noexcept override {
+		if (m_format == MyFormat::kJSON)
+			return "{ \"data\": \"" + m_data + "\" }";
+		else
+			return MyData::print();
+	}
+
+private:
+	MyData::MyFormat m_format = MyData::MyFormat::kJSON;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,19 @@
 #include <fstream>
 #include <memory>
 
-//#include "Data_Originally.hpp"
+#include "Data_Originally.hpp"
 #include "Data_DrySolid.hpp"
 
 
 int main() {
 	setlocale(LC_ALL, "ru_RU.UTF-8");
-	/*std::cout << "\tСвойства хорошего кода. Принципы DRY и SOLID.\n\n" << std::endl;
+	std::cout << "\tСвойства хорошего кода. Принципы DRY и SOLID.\n\n" << std::endl;
+
+	std::ofstream out_file("out.txt", std::ios::out);
+	std::string str = "Hello World!";
 
 	{
 		std::cout << "\tОперации с объектом класса с оригинальным кодом:\n" << std::endl;
-		std::ofstream out_file("out.txt", std::ios::out);
-		std::string str = "Hello World!";
 
 		Data data(str, Data::Format::kText);
 		std::cout << "Создан объект со строкой '" + str + "' и форматом 'kText'.\n" << std::endl;
@@ -77,23 +78,30 @@ int main() {
 		}
 	}
 
-	std::cout << std::endl << std::endl;*/
+	std::cout << std::endl << std::endl;
 
 	{
 		std::cout << "\tОперации с объектом класса после применения принципов DRY и SOLID:\n" << std::endl;
-		std::ofstream out_file("out.txt", std::ios::out);
-		std::string str = "Hello World!";
 
 		std::unique_ptr<MyData> p_data = std::make_unique<Text>(str);
 		std::cout << "Создан объект 'Text' со строкой '" + str + '\n' << std::endl;
 		std::cout << "Вызов метода 'print()':" << std::endl;
 		std::cout << p_data->print() << std::endl;
-		std::cout << "Вызов метода 'print()':" << std::endl;
-		std::cout << p_data->print(MyFormat::kHTML) << std::endl;
 		std::cout << std::endl;
 
+		p_data.reset(new HTML(str));
+		std::cout << "Создан объект 'HTML' со строкой '" + str + '\n' << std::endl;
+		std::cout << "Вызов метода 'print()':" << std::endl;
+		std::cout << p_data->print() << std::endl;
+		std::cout << std::endl;
+
+		p_data.reset(new JSON(str));
+		std::cout << "Создан объект 'JSON' со строкой '" + str + '\n' << std::endl;
+		std::cout << "Вызов метода 'print()':" << std::endl;
+		std::cout << p_data->print() << std::endl;
+		std::cout << std::endl;
 	}
-	
+
 	
 	std::cin.get();
 	return 0;
